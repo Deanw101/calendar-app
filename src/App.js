5fa4ec56084233eedc80bca08bff968e383e0d52
Deanw101 <deanpwithers@gmail.com>
2021-01-11 23:25:49 -0700

    Got a good start at app development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import Datetime from "react-datetime";
+import './dt.css';
 import './App.css';
-
+import Logo from './assets/logo.png';
 function App() {
+
+  const handleBack = () => {
+    window.history.back()
+  }
+
   return (
+
+    <>
+
+
+    <div className="spiral">
+    <div className="logo">
+              <a className="load-spiral" href="index.html">
+                <img src={Logo} />
+              </a>
+            </div>
+
+
+      <div className="review-head">
+
+        <div className="heading-text">
+
+          <div className="review-text-front">
+            <h1>MAKE THE DATE. YOU'RE ONLY CLICKS <br /> AWAY FROM AN UNFORGETTABLE SHOOT.</h1>
+            </div>
+            <div className="middleskew"> </div>
+            <div className="review-text-back">
+              <h1>MAKE THE DATE. YOU'RE ONLY CLICKS <br /> AWAY FROM AN UNFORGETTABLE SHOOT.</h1>
+              </div>
+              </div>
+            </div>
+      </div>
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleBack}>Back</button>
+      <input type="text" />
+      <Datetime />
+      <button>Submit</button>
     </div>
+
+    </>
   );
 }
 
